7586ddf0865c0e4a1a47db2aec47cadb0e2f6907
masterkudi <cudini.andrea@gmail.com>
2023-09-01 00:15:13 +0200

    menu quasi completato

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -6,29 +6,79 @@ export default {
 
 <template>
     <div class="header-cover">
-        <img src="./assets/img/hero-07-2x.jpg" alt="header-bg">
+        <img src="../assets/img/hero-07-2x.jpg" alt="header-bg">
 
-        <!--  <div>
-        <ul class="nav nav-upside-line">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Active</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-            </li>
-        </ul>
-    </div> -->
+        <div class="menu d-flex">
+            <img class="logo" src="../assets/img/author-logo-round.png" alt="">
+            <ul class="navbar text-center">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Me</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Testimonials</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disa" aria-disabled="true">My Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Meetup</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+                        aria-expanded="false">Shop</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Single Product</a></li>
+                        <li><a class="dropdown-item" href="#">Variable Product</a></li>
+                    </ul>
+                </li>
+
+            </ul>
+        </div>
+        <div>
+        </div>
     </div>
 </template>
 
 <style scoped lang="scss">
-.header-cover {
-    width: 100vw;
+.header-cover img {
+    position: absolute;
+    max-width: 100%;
 }
+
+.menu {
+    display: flex;
+    justify-content: space-between;
+}
+
+.menu .logo {
+    width: 10rem;
+    position: relative;
+    padding: 1rem;
+}
+
+.menu .navbar {
+    position: relative;
+    color: #fff;
+    padding: 0 2rem;
+    margin: 0;
+}
+.menu .nav-item {
+    list-style: none;
+    padding: 1rem;
+}
+
+.menu .nav-link {
+    padding-top: 1rem;
+    border-top: 4px solid;
+    
+}
+.menu .nav-item:hover {
+    transition: 1s;
+    color: #F4B429;
+}
+
+
 </style>
