92f157d5b39cda4d152191b1f56f8c43aee64313
masterkudi <cudini.andrea@gmail.com>
2023-08-31 18:28:03 +0200

    impostazione iniziale, inserimento img in cartella

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,12 +1,34 @@
 <script>
-    export default {
+export default {
 
-    }
+}
 </script>
 
 <template>
+    <div class="header-cover">
+        <img src="./assets/img/hero-07-2x.jpg" alt="header-bg">
 
+        <!--  <div>
+        <ul class="nav nav-upside-line">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Active</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            </li>
+        </ul>
+    </div> -->
+    </div>
 </template>
 
 <style scoped lang="scss">
+.header-cover {
+    width: 100vw;
+}
 </style>
