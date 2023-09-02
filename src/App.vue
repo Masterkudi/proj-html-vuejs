03cab9ba53ecee6899de0f8476e790b808b288c6
masterkudi <cudini.andrea@gmail.com>
2023-09-03 00:13:08 +0200

    sezioni 1a, 2a, 3a e 6a del main completate

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,8 @@ export default {
 <template>
   <TheHeader></TheHeader>
   <main>
-    <div class="container-first text-center p-5 m-5">
+    <!-- PRIMA SEZIONE DEL MAIN -->
+    <div class="container-first text-center p-5">
       <div class="my-component">
         <h1>
           More About Damon Vaughn
@@ -24,13 +25,75 @@ export default {
           of
           the multi-million copy bestseller “The story of my life.”
         </p>
-        <button class="bg-success rounded border-0 p-2 text-white">Meet The Author</button>
+        <img src="./assets/img/hover-button2.png" alt="">
       </div>
     </div>
-    <div class="container-sponsor">
+    <!-- SECONDA SEZIONE DEL MAIN -->
+    <div class="container-sponsor text-center">
       <div>
-        
+        <img src="./assets/img/ebay-logotan-2x-400x132.png" alt="">
+        <img src="./assets/img/audible-logotan-2x-400x132.png" alt="">
+        <img src="./assets/img/bb-logotan-2x-400x132.png" alt="">
+        <img src="./assets/img/kindlefire-logotan-2x-400x132.png" alt="">
       </div>
+    </div>
+    <!-- TERZA SEZIONE DEL MAIN -->
+    <div class="container-audiobook">
+      <div class="d-flex justify-content-center">
+        <div class="d-flex">
+          <div>
+            <img src="./assets/img/audible-app-2x-400x418.png" alt="">
+          </div>
+          <div class="my-component audiobook text-center">
+            <h2>New Audiobook</h2>
+            <h3>NO TIME TO READ? NO PROBLEM. LISTEN TO IT</h3>
+            <p class="p text-secondary">Donec fringilla nunc eu turpis dignissim, at <br> euismod sapien tincidunt. Maecenas eleifend
+              congue
+              cursus.
+            </p>
+            <a class="rounded bg-success p-2 mt-4 text-decoration-none text-white" href="#"><i
+                class="fa-brands fa-audible pe-3"></i>Buy This On Audible</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- QUARTA SEZIONE DEL MAIN -->
+    <div class="container-testimonials">
+
+    </div>
+    <!-- QUINTA SEZIONE DEL MAIN -->
+    <div>
+
+    </div>
+    <!-- SESTA SEZIONE DEL MAIN -->
+    <div class="container-subscribe">
+      <div class="d-flex justify-content-center">
+        <div class="d-flex">
+          <div>
+            <div class="my-component subscribe text-center">
+              <h2>Subscribe To My <br> Newsletter</h2>
+              <h3>BE NOTIFIED ABOUT BOOK SIGNING TOUR DATES</h3>
+            </div>
+            <nav class="navbar bg-body-transparent p-3">
+              <div class="container-fluid">
+                <form class="d-flex" role="search">
+                  <input class="form-control p-2 me-3 " type="search" placeholder="Insert Your Email...*" aria-label="Search">
+                  <button class="btn bg-success text-white" type="submit">Subscribe</button>
+                </form>
+              </div>
+            </nav>
+            <p class="p text-center text-secondary">Donec fringilla nunc eu turpis dignissim, at euismod <br> sapien tincidunt.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- SETTIMA SEZIONE DEL MAIN -->
+    <div>
+
+    </div>
+    <!-- OTTAVA SEZIONE DEL MAIN -->
+    <div>
+
     </div>
   </main>
   <TheFooter></TheFooter>
@@ -40,16 +103,61 @@ export default {
 .container-first {
   justify-content: center;
 }
-.my-component {
-  padding: 2rem;
-}
 
 .my-component h1 {
   font-family: 'Kristi', cursive;
   color: #F4B429;
-  font-size: 4rem;
+  font-size: 3rem;
+  margin-bottom: 2rem;
 }
+
+.my-component h2 {
+  font-family: 'Playfair Display', serif;
+  color: #fff;
+  font-size: 3rem;
+}
+
+.my-component h3 {
+  font-size: 1rem;
+  color: #F4B429;
+}
+
 .my-component p {
+  padding: 0rem 5rem;
   font-size: 2rem;
+  margin-bottom: 3rem;
+}
+
+.my-component .p {
+  font-size: 1.2rem;
+  padding: 0;
+  margin: 1rem 4rem;
+}
+
+.container-sponsor {
+  background-color: #FEF7E9;
+  padding: 2rem;
+}
+
+.container-sponsor img {
+  width: 18%;
+  padding: 2rem;
+}
+
+.my-component.audiobook {
+  padding: 3rem;
+  align-items: center;
+}
+
+.container-audiobook {
+  background-image: url(./assets/img/banner.jpg);
+  padding-top: 5rem;
+  padding-left: 15rem;
+  padding-right: 15rem;
+}
+
+.container-subscribe {
+  background-image: url(./assets/img/banner-2.jpg);
+  padding: 5rem 15rem 5rem 15rem;
 }
 </style>
